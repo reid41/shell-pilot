f521fd9d005504177ad1f65a64723f9c58c65693
reid <reid21@qq.com>
2024-05-03 21:19:13 +0800

    add a new pv plugin

--- spilot_common.sh
+++ spilot_common.sh
@@ -8,7 +8,7 @@ OVERWRITE_PROCESSING_LINE="             \033[0K\r"
 COLUMNS=$(tput cols)
 
 # version: major.minor.patch
-SHELL_PILOT_VERSION=1.5.5
+SHELL_PILOT_VERSION=1.6.5
 
 # store directory
 SPILOT_FILES_DEFAULT_DIR=~/spilot_files_dir
